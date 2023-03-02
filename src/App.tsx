b4e549ca01ec8dca45065b04389c7b7a70079fa0
mjanoszek <mjanoszek@protonmail.com>
2023-03-02 23:32:30 +0100

    Splited Calculator logic into separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
-import React, { useState } from 'react';
-import Display from './components/Display/Display';
-import CalculatorButtons from './components/CalculatorButtons/CalculatorButtons';
+import React from 'react';
+import Calculator from './components/Calculator/Calculator';
+
 
 function App() {
-  const [displayValue, setDisplayValue] = useState('');
-
-  const handleButtonClick = (label: string) => {
-    if (label === 'AC') {
-      //todo
-    } else if (label === '+/-') {
-      // todo
-    } else if (label === '%') {
-      //todo
-    } else if (label === '÷') {
-      // tood
-    } else if (label === '×') {
-      //todo
-    } else if (label === '=') {
-      //todo
-    }
-  };
   return (
-    <div className="flex h-screen items-center justify-center font-serif">
-      <div className="flex h-2/3 w-3/4 max-w-sm flex-col items-end justify-end rounded-lg bg-gray-800 shadow-xl">
-        <Display value={displayValue} />
-        <CalculatorButtons onClick={handleButtonClick} />
-      </div>
-    </div>
+    <Calculator/>
   );
 }
 export default App;
---
+++ src/components/Calculator/Calculator.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import Display from '../Display/Display';
+import CalculatorButtons from '../CalculatorButtons/CalculatorButtons';
+
+function Calculator() {
+  const [displayValue, setDisplayValue] = useState('');
+  const [operator, setOperator] = useState('');
+  const [previousValue, setPreviousValue] = useState('');
+
+  const evaluate = (operand1: string, op: string, operand2: string) => {
+    const num1 = parseFloat(operand1);
+    const num2 = parseFloat(operand2);
+
+    switch (op) {
+      case '+':
+        return num1 + num2;
+      case '-':
+        return num1 - num2;
+      case '*':
+        return num1 * num2;
+      case '/':
+        return num1 / num2;
+      default:
+        return 0;
+    }
+  };
+
+
+ 
+  
+
+  const handleButtonClick = (label: string) => {
+    switch (label) {
+      case 'AC':
+        setDisplayValue('');
+        setOperator('');
+        setPreviousValue('');
+        break;
+      case '+/-':
+        setDisplayValue(prevValue => (parseFloat(prevValue) * -1).toString());
+        break;
+      case '%':
+        setDisplayValue(prevValue => (parseFloat(prevValue) / 100).toString());
+        break;
+      case '÷':
+        setDisplayValue('');
+        setOperator('/');
+        setPreviousValue(displayValue);
+        break;
+      case '×':
+        setOperator('*');
+        setPreviousValue(displayValue);
+        setDisplayValue('');
+        break;
+      case '-':
+        setOperator('-');
+        setPreviousValue(displayValue);
+        setDisplayValue('');
+        break;
+      case '+':
+        setOperator('+');
+        setPreviousValue(displayValue);
+        setDisplayValue('');
+        break;
+      case '=':
+        if (operator && displayValue !== '') {
+          const result = evaluate(previousValue, operator, displayValue);
+          setDisplayValue(result.toString());
+          setOperator('');
+          setPreviousValue('');
+        }
+        break;
+      default:
+        setDisplayValue(prevValue => {
+          if (prevValue === '0') {
+            return label;
+          } else {
+            return prevValue + label;
+          }
+        });
+        break;
+    }
+
+    
+  };
+  
+
+  return (
+    <div className="flex h-screen items-center justify-center font-serif">
+      <div className="flex h-2/3 w-3/4 max-w-sm flex-col items-end justify-end rounded-lg bg-[#212121] shadow-xl">
+        <Display value={displayValue} />
+        <CalculatorButtons onClick={handleButtonClick} />
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
