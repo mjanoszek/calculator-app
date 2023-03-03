d3ccf16d7bbe3509888aed710b5b82dd506b7473
mjanoszek <mjanoszek@protonmail.com>
2023-03-03 21:40:52 +0100

    Added max width class to adjust displaying numbers on mobile devices

--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -7,10 +7,10 @@ interface DisplayProps {
 function Display(props: DisplayProps) {
 
   return (
-    <div className="flex items-center justify-end m-2 px-4 py-2 text-6xl font-medium text-white">
-      <h1>
-        {props.value}
-      </h1>
+    <div className="flex items-center justify-end m-2 px-4 py-2 text-6xl font-medium text-white max-[375px]:text-3xl">
+      <h1> 
+       {props.value}
+     </h1>
     </div>
   );
 }
