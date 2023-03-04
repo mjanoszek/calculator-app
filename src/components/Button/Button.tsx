76e74e99325a60cba1e9505ac3a722db0595e7ee
mjanoszek <mjanoszek@protonmail.com>
2023-03-04 07:34:21 +0100

    Shortened and simplified classnames

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -15,29 +15,29 @@ function Button(props: ButtonProps) {
   const handleClick = () => props.onClick(props.label);
 
 
+  const baseStyle = 'w-1/4 p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1';
+
  
   const buttonStyles: ButtonStylesProps  = {
-    'AC': ' w-1/4 bg-dark-gray p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1',
-    '+/-' : 'w-1/4 bg-dark-gray p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1',
-    '%' : ' w-1/4 bg-dark-gray p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1',
-    '÷' : ' w-1/4 bg-orange p-5 max-[370px]:text-lg text-3xl text-white outline  outline-black outline-1',
-    '×' : ' w-1/4 bg-orange p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1',
-    '-' : ' w-1/4 bg-orange p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1',
-    '+' : ' w-1/4 bg-orange p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1',
-    '=': ' w-1/4 bg-orange p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1 rounded-br-md',
-    '7' : ' w-1/4 bg-gray p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1',
-    '8' : ' w-1/4 bg-gray p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1',
-    '9' : ' w-1/4 bg-gray p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1',
-    '4' : ' w-1/4 bg-gray p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1',
-    '5' : ' w-1/4 bg-gray p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1',
-    '6' : ' w-1/4 bg-gray p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1',
-    '1' : ' w-1/4 bg-gray p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1',
-    '2' : ' w-1/4 bg-gray p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1',
-    '3' : ' w-1/4 bg-gray p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1',
-    '0': ' flex-1 w-1/4 rounded-bl-md bg-gray p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1',
-    '.' : 'w-1/4 bg-gray p-5 max-[370px]:text-lg text-3xl text-white outline outline-black outline-1',
-    
-
+    'AC':  `${baseStyle} bg-dark-gray`,
+    '+/-': `${baseStyle} bg-dark-gray`,
+    '%' :  `${baseStyle} bg-dark-gray`,
+    '÷' :  `${baseStyle} bg-orange`,
+    '×' :  `${baseStyle} bg-orange`,
+    '-' :  `${baseStyle} bg-orange`,
+    '+' :  `${baseStyle} bg-orange`,
+    '=' :  `${baseStyle} bg-orange rounded-br-md`,
+    '7' :  `${baseStyle} bg-dark-gray`,
+    '8' :  `${baseStyle} bg-dark-gray`,
+    '9' :  `${baseStyle} bg-dark-gray`,
+    '4' :  `${baseStyle} bg-dark-gray`,
+    '5' :  `${baseStyle} bg-dark-gray`,
+    '6' :  `${baseStyle} bg-dark-gray`,
+    '1' :  `${baseStyle} bg-dark-gray`,
+    '2' :  `${baseStyle} bg-dark-gray`,
+    '3' :  `${baseStyle} bg-dark-gray`,
+    '0' :  `${baseStyle} bg-dark-gray flex-1 w-1/4 rounded-bl-md`,
+    '.' :  `${baseStyle} bg-dark-gray`,
   };
 
   return (
